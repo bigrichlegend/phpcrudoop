647e9bc8f00454c9f265542d8da9b4fbfd26904b
Richard McPherson <bigrichlegend@ymail.com>
2016-11-03 11:18:18 +0000

    Updates to mysqldb
    Build query method and make more generic

---
+++ phpcrudoop/MySqlDb.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LordBic
+ * Date: 02/11/2016
+ * Time: 21:14
+ */
+class MySqlDb {
+	//  Create protected property since only method in class and any possible
+	//  child class will need to access it
+	protected $_mysqli;
+	protected $_query;
+	protected $_where = array();
+	protected $_paramTypeList;
+
+	//Constructor
+	public function __construct( $host, $username, $password, $db ) {
+		$this->_mysqli = new mysqli( $host, $username, $password, $db ) or die( 'Error connecting to database' );
+	}
+
+	public function query( $query ) {
+		$this->_query = filter_var( $query, FILTER_SANITIZE_STRING );
+		$stmt         = $this->_prepareQuery();
+		$stmt->execute();
+		$results = $this->_dynamicBindResults( $stmt );
+
+		return $results;
+
+	}
+
+	public function get( $tableName, $numRows = null ) {
+		$this->_query = "SELECT * FROM $tableName";
+		$stmt         = $this->_buildQuery( $numRows );
+		$stmt->execute();
+		$results = $this->_dynamicBindResults( $stmt );
+
+		return $results;
+	}
+
+	public function insert( $tableName, $insertData ) {
+
+	}
+
+	public function update( $tableName, $tableData ) {
+
+	}
+
+	public function delete( $tableName ) {
+
+	}
+
+	public function where( $whereProp, $whereValue ) {
+		$this->_where[ $whereProp ] = $whereValue;
+
+	}
+
+	protected function _buildQuery( $numRows = null, $tableData = false ) {
+		$hasTableData = null;
+		//if a tableData was passed in as part of the build query then check if it
+		// of type array
+		if ( gettype( $tableData ) === 'array' ) {
+			$hasTableData = true;
+		}
+
+		//Did user call where
+		if ( ! empty( $this->_where ) ) {
+			$keys        = array_keys( $this->_where );
+			$where_prop  = $keys[0];
+			$where_value = $this->_where[ $where_prop ];
+
+			if ( $hasTableData ) {
+				// Loop through array and get key => value pair
+				foreach ( $tableData as $prop => $value ) {
+					//loop
+				}
+			} else {
+				$this->_paramTypeList = $this->_determineType( $where_value );
+				$this->_query .= " WHERE " . $where_prop . "= ?";
+			}
+		}
+		if ( isset( $numRows ) ) {
+			$this->_query .= " LIMIT " . (int) $numRows;
+		}
+		// We now prepare query
+		$stmt = $this->_prepareQuery();
+		echo "LEBERT PORTER4";
+
+		if ( $this->_where ) {
+			$stmt->bind_param( $this->_paramTypeList, $where_value );
+		}
+		return $stmt;
+
+	}
+
+
+	protected function _determineType( $item ) {
+		$param_type = '';
+		switch ( gettype( $item ) ) {
+			case 'string' :
+				$param_type = 's';
+				break;
+			case 'integer' :
+				$param_type = 'i';
+				break;
+			case 'blob' :
+				$param_type = 'b';
+				break;
+			case 'double' :
+				$param_type = 'd';
+				break;
+		}
+		return $param_type;
+	}
+
+	protected function _dynamicBindResults( $stmt ) {
+		$parameters = array();
+		$results    = array();
+		$meta       = $stmt->result_metadata();
+		while ( $field = $meta->fetch_field() ) {
+			$parameters[] = &$row[ $field->name ];
+		}
+		call_user_func_array( array( $stmt, 'bind_result' ), $parameters );
+		while ( $stmt->fetch() ) {
+			//echo "<pre>",print_r($row),"</pre>";
+			$x = array();
+			foreach ( $row as $key => $val ) {
+				//echo "The key is : ". $key . " and the value is :".$val ."<br>";
+				$x[ $key ] = $val;
+			}
+			$results[] = $x;
+		}
+		return $results;
+	}
+
+	protected function _prepareQuery() {
+		if ( ! $stmt = $this->_mysqli->prepare( $this->_query ) ) {
+			trigger_error( 'Problem preparing query', E_USER_ERROR );
+		}
+			return $stmt;
+	}
+
+	public function __destruct() {
+		$this->_mysqli->close();
+	}
+
+}
---
+++ phpcrudoop/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once( 'MySqlDb.php' );
+$Db      = new MySqlDb( 'localhost', 'root', 'root', 'records' );
+$results = $Db->query( 'SELECT * FROM players' );
+//echo "<pre>",print_r($results),"</pre>";
+//$Db->where('id', 10);
+$output = $Db->get('players', 5);
+echo "Lord Bic says :".print_r($output);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PHP CRUD OOP</title>
+	<meta http-equiv="content-type" content="text/html charset=utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<table id="players">
+	<thead>
+	<th>ID</th>
+	<th>First Name</th>
+	<th>Last Name</th>
+	<th>Edit</th>
+	<th>Delete</th>
+	</thead>
+	<?php
+	foreach ( $results as $result ) {
+		echo "<tr>";
+		echo "<td>" . $result['id'] . "</td>";
+		echo "<td>" . $result['firstname'] . "</td>";
+		echo "<td>" . $result['lastname'] . "</td>";
+		echo "<td><a href='" . $result['id'] . "'>Edit</a></td>";
+		echo "<td><a href='" . $result['id'] . "'>X</a></td>";
+		echo "</tr>";
+	}
+	?>
+</table>
+</body>
+</html>
